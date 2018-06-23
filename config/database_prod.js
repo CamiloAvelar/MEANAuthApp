59743a015de606eae89262dd5198d75843788f63
Camilo Avelar <camilo.avelar@gmail.com>
2018-06-23 14:34:51 -0300

    Deploying

--- config/database_prod.js
+++ config/database_prod.js
@@ -1,4 +1,4 @@
 module.exports = {
-    database: process.env.MONGO_URI, //Put here the address to your Database
-    secret: process.env.SECRET_OR_KEY //Put here the secret
+    database: process.env.DATABASE, //Put here the address to your Database
+    secret: process.env.SECRET //Put here the secret
   };
